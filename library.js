623db26f8e6bf75a52d99906f83ea759ffdbd0d0
Sumaer Jolly <sumaerjolly@gmail.com>
2019-11-14 15:37:31 +0530

    basic html and js set up added

--- library.js
+++ library.js
@@ -1,1 +1,33 @@
-alert("Connected!")
+// var tableRow = document.querySelectorAll("tr");
+// var tableData = document.querySelectorAll("td");
+// var addButton = document.querySelector(".add");
+
+
+// let myLibrary = [];
+
+// function Book(title,author,pages) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+// }
+
+// var newBook = new Book("Test Book","Othmane","100");
+// var secondBook = new Book("Second Book","Sumaer","100");
+
+// function addBookToLibrary(book) {
+//     myLibrary.push(book)
+// }
+
+// addBookToLibrary(newBook);
+// addBookToLibrary(secondBook);
+
+// addButton.addEventListener("click",function(){
+    
+// })
+
+// for(var i = 0; i < myLibrary.length; i++){
+//     tableData.textContent = (myLibrary[i].title);
+// }
+
+
+
