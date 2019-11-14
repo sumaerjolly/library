f42f718ffa3cef8b53a089ccffb86574e79666b6
othmane <othman-19@hotmail.fr>
2019-11-14 13:12:31 +0100

    Render the data from library array to html table

--- library.js
+++ library.js
@@ -3,23 +3,76 @@
 // var addButton = document.querySelector(".add");
 
 
-// let myLibrary = [];
+const myLibrary = [];
 
-// function Book(title,author,pages) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-// }
+function Book(title,author,pages,status = "unread") {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
 
-// var newBook = new Book("Test Book","Othmane","100");
-// var secondBook = new Book("Second Book","Sumaer","100");
+let newBook = new Book("Test Book","Othmane","100");
+let secondBook = new Book("Second Book","Sumaer","100");
 
-// function addBookToLibrary(book) {
-//     myLibrary.push(book)
-// }
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
 
-// addBookToLibrary(newBook);
-// addBookToLibrary(secondBook);
+addBookToLibrary(newBook);
+addBookToLibrary(secondBook);
+
+function render(table, library) {
+    for (let book of library) {
+      let row = table.insertRow();
+      for (let key in book) {
+        let cell = row.insertCell();
+        if(key === "status"){
+            let button = document.createElement("BUTTON");
+            button.innerHTML = book[key];
+            cell.appendChild(button);
+        }else{
+            let text = document.createTextNode(book[key]);
+            cell.appendChild(text);
+        }
+        
+      }
+    }
+  }
+  let table = document.querySelector("table");
+  render(table, myLibrary);
+  let selectedButton = document.querySelectorAll("button")
+  
+//   for(let i = 0; i < selectedButton.length; i++){
+//     selectedButton[i].addEventListener("click",function(){
+//         if(this.textContent === "read"){
+//             this.classList.remove("selected");
+//             this.textContent = "unread";
+//         }
+//         else{
+//             this.classList.add("selected");
+//             this.textContent = "read";
+//         }
+       
+//     })
+// }  
+
+  selectedButton.forEach(btn => btn.addEventListener("click", function(){
+    if(this.textContent == "read"){
+        this.classList.remove("selected");
+        this.textContent = "unread";
+    }
+    else{
+        this.classList.add("selected");
+        this.textContent = "read";
+    }
+   
+}));
+  
+
+
+
+
 
 // addButton.addEventListener("click",function(){
     
